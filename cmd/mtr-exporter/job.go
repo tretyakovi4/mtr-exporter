5db1cc5addc122ae13f7a0969138df02f4597354
tretyakovi4 <mr.tretyakovi4@gmail.com>
2022-06-07 17:00:07 +0400

    modified:   cmd/mtr-exporter/job.go

--- cmd/mtr-exporter/job.go
+++ cmd/mtr-exporter/job.go
@@ -24,7 +24,7 @@ type mtrJob struct {
 
 func newMtrJob(mtr string, args []string) *mtrJob {
 	extra := []string{
-		"-j", // json output
+		"-j -rc 10", // json output
 	}
 	args = append(extra, args...)
 	cmd := exec.Command(mtr, args...)
